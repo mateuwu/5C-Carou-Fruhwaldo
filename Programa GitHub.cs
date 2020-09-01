82bf0fc89300d5e823109e7ddf08fba08fd6783d
Mateo Fernandez Carou <70227125+mateuwu@users.noreply.github.com>
2020-09-01 15:53:21 -0300

    Add files via upload

---
+++ Programa GitHub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, prom;
+            Console.WriteLine("Numero 1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero 2:");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Numero 3:");
+            num3 = int.Parse(Console.ReadLine());
+            prom = (num1 + num2 + num3) / 3;
+            Console.WriteLine("Promedio: {0}", prom);
+            Console.ReadLine();
+            if (prom >= 7)
+            {
+                Console.WriteLine("El alumno está aprobado");
+                Console.ReadLine();
+            }
+            else
+            {
+                    Console.WriteLine("El alumno irá a diciembre");
+                    Console.ReadLine();
+                }
+                
+
+
+                    
+                
+            
+        }
+
+    }
+}
