46ef83b2b6cd3b5c6dacd670302f14ef382f7b83
Fruhwaldo <70227427+Fruhwaldo@users.noreply.github.com>
2020-09-01 18:55:02 -0300

    Update Programa GitHub.cs

--- Programa GitHub.cs
+++ Programa GitHub.cs
@@ -10,17 +10,18 @@ namespace ConsoleApp55
     {
         static void Main(string[] args)
         {
-            int n1, n2, n3, prom;
+            int n1, n2, n3, promfinal;
             Console.WriteLine("Numero 1:");
             n1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Numero 2:");
             n2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Numero 3:");
             n3 = int.Parse(Console.ReadLine());
-            prom = (n1 + n2 + n3) / 3;
-            Console.WriteLine("Promedio: {0}", prom);
+            promfinal = (n1 + n2 + n3) / 3;
+            Console.WriteLine("Promedio: {0}", promfinal);
             Console.ReadLine();
-            if (prom >= 7)
+
+            if (promfinal >= 7)
             {
                 Console.WriteLine("El alumno aprobo");
                 Console.ReadLine();
